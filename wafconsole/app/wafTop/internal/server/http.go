40a0c19f0a9d22b60471e2c008a9c919e7c59732
zhangyunfei <zhangyunfei>
2024-10-16 08:46:12 +0800

    feat: new add protovalidate

--- wafconsole/app/wafTop/internal/server/http.go
+++ wafconsole/app/wafTop/internal/server/http.go
@@ -4,17 +4,25 @@ import (
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/http"
+	"log/slog"
 	v1 "wafconsole/api/wafTop/v1"
 	"wafconsole/app/wafTop/internal/conf"
+	"wafconsole/app/wafTop/internal/server/plugin"
 	rule "wafconsole/app/wafTop/internal/service/rule"
 	site "wafconsole/app/wafTop/internal/service/site"
 )
 
 // NewHTTPServer new an HTTP server.
 func NewHTTPServer(c *conf.Server, appWafTop *site.WafAppService, serverWaf *site.ServerService, buildRule *rule.BuildRuleService, logger log.Logger) *http.Server {
+	protoValidate, err := plugin.NewValidate()
+	if err != nil {
+		slog.Error("protoValidate", err)
+		return nil
+	}
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
+			protoValidate.ValidateUnaryServerInterceptor(), //参数校验
 		),
 	}
 	if c.Http.Network != "" {
