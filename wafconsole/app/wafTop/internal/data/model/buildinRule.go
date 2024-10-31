8ac909e507fffae60e6455b1ef09531349cf4c51
zhangyunfei <zhangyunfei>
2024-10-31 12:08:42 +0800

    fix:init mysql ,etcd failed

--- wafconsole/app/wafTop/internal/data/model/buildinRule.go
+++ wafconsole/app/wafTop/internal/data/model/buildinRule.go
@@ -2,7 +2,6 @@ package model
 
 import (
 	"gorm.io/gorm"
-	"log/slog"
 )
 
 const TableNameBuildinRule = "waf_buildin_rule"
@@ -18,15 +17,4 @@ type BuildinRule struct {
 
 func (BuildinRule) TableName() string {
 	return TableNameBuildinRule
-}
-
-// AfterCreate 在建表操作完成之后
-func (BuildinRule) AfterCreate(tx *gorm.DB) (err error) {
-	err = tx.Exec("INSERT INTO waf_buildin_rule (id , created_at, updated_at, name, description, group_id, seclang  , risk_level) VALUES (90001 , NOW() , NOW() , 'coraza基础配置', 'waf内核的基础配置文件' , 1 , 'Include wafCoraza/ruleset/coraza.conf' , 1 , 4),(90002 , NOW() , NOW() , 'coraza的内置防护规则1' , 'coraza内置防护规则的文件' , 1 , 'Include wafCoraza/ruleset/coreruleset/crs-setup.conf.example' , 1 , 4),(9003 , NOW() , NOW() , 'coraza的内置防护规则2' , 'coraza内置防护规则文件' , 1 , 'Include wafCoraza/ruleset/coreruleset/rules/*.conf' , 1 , 4);").Error
-	if err != nil {
-		slog.Error("buildinRule AfterCreate err:", err.Error())
-		return err
-	}
-	return nil
-
 }
